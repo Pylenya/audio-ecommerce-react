19d9fe8950d5596118ba51840f42e2a9d7e97c75
Леонид <lenya.zverevich@gmail.com>
2023-08-26 14:03:47 +0300

    add zx7speaker section

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 import ProductList from "../ProductList/ProductList";
 import ZX9Speaker from "../ZX9Speaker/ZX9Speaker";
+import ZX7Speaker from "../ZX7Speaker/ZX7Speaker";
 export const Home = () => {
   return (
     <>
@@ -10,6 +11,7 @@ export const Home = () => {
       <Hero />
       <ProductList />
       <ZX9Speaker />
+      <ZX7Speaker />
     </>
   );
 };
---
+++ src/components/ZX7Speaker/ZX7Speaker.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import desctopIMG from "../../images/home/desktop/image-speaker-zx7.jpg";
+import tabletIMG from "../../images/home/tablet/image-speaker-zx7.jpg";
+import mobileIMG from "../../images/home/mobile/image-speaker-zx7.jpg";
+import "./zx7speaker.scss";
+import Button from "../Button/Button";
+function ZX7Speaker() {
+  return (
+    <section className="zx7speaker">
+      <div className="container">
+        <div className="zx7speaker__box">
+          <picture>
+            <source media="(max-width:480px)" srcset={mobileIMG} />
+            <source media="(max-width:999px)" srcset={tabletIMG} />
+            <img
+              className="zx7speaker__img"
+              src={desctopIMG}
+              alt="zx7speaker"
+            />
+          </picture>
+          <div className="zx7speaker__text">
+            <h2 className="zx7speaker__title">ZX7 SPEAKER</h2>
+            <Button>See product</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ZX7Speaker;
